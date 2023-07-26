5a8e7407f08c5ffbb1d546197b6c5273010b5ba9
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-07-26 21:28:39 +0100

    added .eslint to the project

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   configureWebpack: config => {
     config.plugins.push(AutoImport({
       resolvers: [ElementPlusResolver()]
