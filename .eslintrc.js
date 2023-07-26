301f3bd405afc83f5151f7aabada9b560c9056a7
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-07-26 18:05:48 +0100

    added .eslint to the project

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+  ],
+  rules: {
+    // Add your project-specific ESLint rules here, if needed
+  },
+};
